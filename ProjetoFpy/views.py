c33755162970d098af35f569156b49a7a02a31bb
teleco-deDeus <telecobelmiro@gmail.com>
2026-06-10 21:44:59 -0300

    estrutura base e pa

--- ProjetoFpy/views.py
+++ ProjetoFpy/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def tela_inicial(request):
+    return render(request, 'home.html')
+
+def login_view(request):
+    return render(request, 'login.html') 
+
+def cadastro_view(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome')
+        cpf_input = request.POST.get('cpf')
+        senha = request.POST.get('senha')
+        conf_senha = request.POST.get('confirmar_senha')
+
+        # 1. Limpa o CPF (deixa só números)
+        cpf_limpo = ''.join(filter(str.isdigit, cpf_input))
+
+        erros = False
+
+        # Validação das senhas
+        if senha != conf_senha:
+            messages.error(request, "As senhas não coincidem!")
+            erros = True
+        
+        # Validação do tamanho do CPF
+        elif len(cpf_limpo) != 11:
+            messages.error(request, "O CPF deve ter exatamente 11 números.")
+            erros = True
+        
+        # Validação de números repetidos (Ex: 11111111111)
+        elif cpf_limpo == cpf_limpo[0] * 11:
+            messages.error(request, "CPF inválido! Números repetidos não permitidos.")
+            erros = True
+
+        # Se passou em tudo sem erros, redireciona para a tela de login
+        if not erros:
+            return redirect('login')
+
+    return render(request, 'cadastro.html')
--- setup/urls.py
+++ setup/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for setup project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from ProjetoFpy import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Rota da Página Inicial (Ex: http://127.0.0.1:8000/)
+    path('', views.tela_inicial, name='tela_inicial'),
+    
+    # Rota da Página de Login (Ex: http://127.0.0.1:8000/login/)
+    path('login/', views.login_view, name='login'),
+    
+    # Rota da Página de Cadastro (Ex: http://127.0.0.1:8000/cadastro/)
+    path('cadastro/', views.cadastro_view, name='cadastro'),
 ]
